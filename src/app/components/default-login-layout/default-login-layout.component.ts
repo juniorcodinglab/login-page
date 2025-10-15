ece46c9b449a4409f6265e3e260c1e1fcd3ff857
Silvio Cesar <silvio_cbsj@hotmail.com>
2025-10-15 07:14:49 -0300

    feat: create signup and socia media buttons

--- src/app/components/default-login-layout/default-login-layout.component.ts
+++ src/app/components/default-login-layout/default-login-layout.component.ts
@@ -8,13 +8,12 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrl: './default-login-layout.component.scss'
 })
 export class DefaultLoginLayoutComponent {
-
   @Input() title: string = "";
   @Input() primaryBtnText: string = "";
   @Input() secondaryBtnText: string = "";
   @Input() disablePrimaryBtn: boolean = true;
-  
   @Output("submit") onSubmit = new EventEmitter();
+
   @Output("navigate") onNavigate = new EventEmitter();
   
   submit(){
